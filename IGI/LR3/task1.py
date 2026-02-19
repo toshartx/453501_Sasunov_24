88035b821dc34c0dce71ef18cb33c088ac56023f
Anton <tosharevivetx@gmail.com>
2026-02-19 19:13:47 +0000

    solve task1 function

---
+++ IGI/LR3/task1.py
@@ -0,0 +1,25 @@
+import math
+
+def solve_task1(x, eps = 1e-5) -> float:
+    result: float = 0.0
+    n: int = 1       
+    ln2: float = math.log(2)   # iteration counter
+
+    arg: float = 1 - x         # ln argument
+
+    if arg < 0: 
+        raise ValueError("ln arg less than 0")
+
+    while arg >= 2:      
+        arg /= 2
+        result += ln2
+
+    adder: float = 1 - arg           # var that adding to result
+    while n < 1000 and abs(adder) >= eps:
+        adder = pow(1 - arg, n) / float(n)
+        result -= adder
+        n += 1
+
+    return result, n
+
+
