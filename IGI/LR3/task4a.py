78db485154fe03e9b0f8fd80efcc9fc43c2ca089
Anton <tosharevivetx@gmail.com>
2026-03-13 13:54:06 +0000

    upd task4a

--- IGI/LR3/task4a.py
+++ IGI/LR3/task4a.py
@@ -1,7 +1,7 @@
 def solve_task4a(string: str) -> int:
     counter: int = 0
 
-    string.replace(",", " ")            # replacing , on spaces
+    string.replace(",","")            # delete ,
     for word in string.split():        # splitting string
         if word.count < 6:
             counter += 1
