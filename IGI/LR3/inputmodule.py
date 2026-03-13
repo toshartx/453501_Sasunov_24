b1a93a2d10cdb523f9a89623adbd0ce7c5895673
Anton <tosharevivetx@gmail.com>
2026-03-13 11:05:02 +0000

    finished v1.0 task2 and inputmodule.py

---
+++ IGI/LR3/inputmodule.py
@@ -0,0 +1,22 @@
+def correct_input(datatype: type = str):
+    while True:
+        inputed = input()
+        try:
+            return datatype(inputed)
+        except ValueError:
+            print("Ошибка ввода! Повторите попытку!")
+            continue
+
+def input_nums():
+    nums: list = []
+    while True:
+        num = correct_input(int)
+        if num == 0:
+            return nums
+        nums.append(num)
+
+def gen_nums():
+    num = 1                     # ???
+    while num != 0:
+        num = correct_input(int)
+        yield num
---
+++ IGI/LR3/task2.py
@@ -0,0 +1,11 @@
+from inputmodule import input_nums
+from inputmodule import gen_nums
+def solve_task2(nums) -> int:
+    nums_in_range: int = 0
+    for num in nums:
+        if num in range(5, 25):
+            nums_in_range += 1
+    return nums_in_range
+
+nums = gen_nums()
+print(solve_task2(nums))
