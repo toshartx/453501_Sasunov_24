46f5ff2c3a93ce36c0eae5e667b22e668689d9a0
Anton <tosharevivetx@gmail.com>
2026-04-29 23:29:13 +0000

    some changes in task4

--- IGI/LR4/Task4/figures/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Пакет с классами геометрических фигур"""
-
-from .abstract_figure import GeometricFigure
-from .color import FigureColor
-from .trapezoid import IsoscelesTrapezoid
-
-__all__ = ['GeometricFigure', 'FigureColor', 'IsoscelesTrapezoid']
